159bbbe812038207620112e077080f3bea63d44a
Charlie Chen <conechan@gmail.com>
2017-02-01 22:37:47 +0800

    ep 16.

--- app/Post.php
+++ app/Post.php
@@ -4,5 +4,14 @@ namespace App;
 
 class Post extends Model
 {
-    //
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+
+        $this->comments()->create(compact('body'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
+
+Route::post('/posts/{post}/comments', 'CommentsController@store');
